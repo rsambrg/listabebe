41d49cf820ae4a0aa0277d86d3fa65455ef18d3d
rsambrg <beca@writeme.com>
2023-09-08 00:25:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ sobre.addEventListener("click", function(){
 });
 
 perguntas.addEventListener("click", function(){
-    conteudo.innerHTML = "<h2> Perguntas Frequentes </h2><big> <big> <p>P: Quantos meses de gestação? <br> R: 7 meses. <br><br>P: Qual vai ser o nome dele? <br> R: Joaquim, mas a gente chama de Kim, Quinzinho e Jojo!<br><br>P: E quando ele nasce?<br>R:Eu completo 40 semanas a partir do dia 19/11.<br> Temos um bolão para ver quem vai acertar a data e se você quiser participar, mande seu palpite para: quediavounascer@mail.com</p>"
+    conteudo.innerHTML = "<h2> Perguntas Frequentes </h2><big> <big> <p>P: Quantos meses de gestação? <br> R: 7 meses. <br><br>P: Qual vai ser o nome dele? <br> R: Joaquim, mas a gente chama de Kim, Quinzinho e Jojo!<br><br>P: E quando ele nasce?<br>R:Eu completo 40 semanas a partir do dia 19/11.<br> Temos um bolão para ver quem vai acertar a data e se você quiser participar, mande seu palpite para: quediavounascer@mail.com<br><br>P:Posso mandar Pix ao invés de comprar o presente?<br>R:Pode sim, é só falar comigo no whatsapp que eu passo a chave e tá tudo certo!Sei que não é todo mundo que vai ter tempo pra escolher/comprar presente!!<br><br>P:Cliquei nos nomes na página inicial e o link não abriu pra comprar!<br>R:Os nomes na página inicial não são links, são apenas uma lista pra sugestão de presentes!Os links vão estar na aba Links de compra e são apenas sugestões, você não precisa necessariamente comprar o que eu coloquei, aquilo é so uma guia para quem tá perdido sobre o que comprar!</p>"
 });
 
 bolao.addEventListener("click",function(){
